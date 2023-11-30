a8e5d1daf11f5dc1906bf243aaba0df5fc7f5eb8
HARIKRISHNAN RK <harikrishnan8888888@gmail.com>
2023-11-30 10:47:12 -0800

    added code for is balaced and is subtree

---
+++ isBalanced.py
@@ -0,0 +1,12 @@
+class Solution:
+	def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+		def dfs(root):
+			if not root:
+				return [True, 0]
+			left, right = dfs(root.left), dfs(root.right)
+			balance = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+			return [balance, 1 + max(left[1], right[1])]
+
+		res = dfs(root)
+		return res[0]
